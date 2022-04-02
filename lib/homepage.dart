45b7e1d383a9fae1ac249f0537d1899f8a312ebb
Zachary Holbrook <zackholbrook44@gmail.com>
2022-04-02 01:00:22 -0400

    initial flutter app with keyboard func

---
+++ lib/homepage.dart
@@ -0,0 +1,37 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //var to store user type
+  final _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        TextField(
+          controller: _textController,
+          decoration: InputDecoration(
+              hintText: 'Hello there holby',
+              border: const OutlineInputBorder(),
+              suffixIcon: IconButton(
+                  onPressed: () {
+                    //clear text
+                    _textController.clear();
+                  },
+                  icon: const Icon(Icons.clear))),
+        )
+      ]),
+    ));
+  }
+}
