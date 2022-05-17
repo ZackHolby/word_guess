f211a8ee9c2473a8adca938370da09c9bda01d0c
Zachary Holbrook <zackholbrook44@gmail.com>
2022-05-17 09:45:01 -0400

    non working state, creating structure for app

---
+++ lib/app/app_colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const Color correctColor = Color(0xFF538D4E);
+const Color wrongColor = Color(0xFF538D4E);
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,6 @@
 import 'package:flutter/material.dart';
-import 'homepage.dart';
-import 'pincode.dart';
+import 'app/app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        backgroundColor: Colors.white,
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      home: const PinCodeVerificationScreen(
-          phoneNumber:
-              "+8801376221100"), // a random number, please don't call xD
-    );
-  }
+  runApp(const App());
 }
